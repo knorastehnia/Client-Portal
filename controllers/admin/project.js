88f7712a84f385d7b48ed094e5a9c0981917fab8
Karan <k.norastehnia@gmail.com>
2025-07-17 18:34:42 -0400

    Admin CRUD capabilities on projects

--- controllers/admin/project.js
+++ controllers/admin/project.js
@@ -1,26 +1,25 @@
-const { queryResult } = require('pg-promise')
 const { db } = require('../../stores/postgres.js')
 
 
 const create_project = async (req, res) => {
     const title = req.body.title
-    const admin_id = req.user_id
+    const admin_id = req.admin_id
 
     try {
         await db.none(`
             INSERT INTO projects (admin_id, title)
             VALUES ($1, $2)
         `, [admin_id, title])
+
+        return res.status(200).send('Project created')
     } catch (err) {
         console.log(err)
-        return res.status(401).send('Something went wrong')
+        return res.status(401).send('Failed to create project')
     }
-
-    return res.status(200).send('Project created successfully')
 }
 
 const get_project_headers = async (req, res) => {
-    const admin_id = req.user_id
+    const admin_id = req.admin_id
 
     try {
         const query_result = await db.any(`
@@ -29,13 +28,73 @@ const get_project_headers = async (req, res) => {
         `, [admin_id])
 
         console.log(query_result)
-        return res.status(200).send('Projects:')
+        return res.status(200).send('Projects retrieved')
     } catch (err) {
         console.log(err)
         return res.status(401).send('Failed to retrieve projects')
     }
 }
 
+const get_project = async (req, res) => {
+    const admin_id = req.admin_id
+    const project_id = req.query.project_id
+
+    try {
+        const query_result = await db.one(`
+            SELECT * FROM projects
+            WHERE admin_id = $1 AND id = $2
+        `, [admin_id, project_id])
+
+        console.log(query_result)
+        return res.status(200).send('Project retrieved')
+    } catch (err) {
+        console.log(err)
+        return res.status(401).send('Failed to retrieve project')
+    }
+}
+
+const update_project = async (req, res) => {
+    const admin_id = req.admin_id
+    const project_id = req.query.project_id
+    const title = req.body.title
+
+    try {
+        const query_result = await db.one(`
+            UPDATE projects
+            SET title = $3
+            WHERE admin_id = $1 AND id = $2
+            RETURNING title
+        `, [admin_id, project_id, title])
+
+        console.log(query_result)
+        return res.status(200).send('Project updated')
+    } catch (err) {
+        console.log(err)
+        return res.status(401).send('Failed to update project')
+    }
+}
+
+const delete_project = async (req, res) => {
+    const admin_id = req.admin_id
+    const project_id = req.query.project_id
+
+    try {
+        await db.none(`
+            DELETE FROM projects
+            WHERE admin_id = $1 AND id = $2
+        `, [admin_id, project_id])
+
+        return res.status(200).send('Project deleted')
+    } catch (err) {
+        console.log(err)
+        return res.status(401).send('Failed to delete project')
+    }
+}
+
 module.exports = {
-    create_project
+    create_project,
+    get_project_headers,
+    get_project,
+    update_project,
+    delete_project
 }
--- routes/admin/project.js
+++ routes/admin/project.js
@@ -5,6 +5,12 @@ const session = require('../../middleware/session.js')
 
 const router = express.Router()
 
-router.post('/create-project', session.check_session('login', 'admin'), project.create_project)
+router.use(session.check_session('login', 'admin'))
+
+router.post('/create-project', project.create_project)
+router.get('/get-project', project.get_project)
+router.get('/get-project-headers', project.get_project_headers)
+router.put('/update-project', project.update_project)
+router.delete('/delete-project', project.delete_project)
 
 module.exports = router
